a8ed89ab8fa2c5727fcbb97673c9ab03d9a6d2ce
Natalie <natalie.soltis@outlook.com>
2021-11-05 13:49:17 -0400

    adsa

---
+++ Assets/DamageVolume.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageVolume : MonoBehaviour
+{
+    [SerializeField] float damage = 5f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
