edc0b2c8a09826abeaef5a73a11fa3c114850976
Natalie <natalie.soltis@outlook.com>
2021-11-05 16:55:55 -0400

    Messed with follower camera. Gotta get started on the sword.

--- Assets/FollowerCamera.cs
+++ Assets/FollowerCamera.cs
@@ -5,26 +5,25 @@ using UnityEngine;
 public class FollowerCamera : MonoBehaviour
 {
     [SerializeField] PlayerProto player;
-    [SerializeField] float screenPercent;
+    [SerializeField]
+    [Range(0, 100)]
+    public float screenPercentX, screenPercentY;
+    Vector3 desiredPositionOverride;
 
     int screenEdgeW = 200;
     int screenEdgeH = 200;
-    int ew;
-    int eh;
 
     Camera _camera;
     Vector3 _followPosition = Vector3.zero;
+    Vector3 _playerPosition = Vector3.zero;
     bool _isCameraMoving = false;
- 
+
 
     private void Awake()
     {
         TryGetComponent(out _camera);
         _followPosition = transform.position;
-        screenEdgeW = Mathf.FloorToInt((float)Screen.width * (screenPercent / 2) / 100);
-        screenEdgeH = Mathf.FloorToInt((float)Screen.height *  (screenPercent / 2) / 100);
-        eh = screenEdgeH;
-        ew = screenEdgeW;
+
     }
 
     // Update is called once per frame
@@ -32,27 +31,32 @@ public class FollowerCamera : MonoBehaviour
     {
         float moveAmount = 5f;
         // worldtoscreenpoint is a ratio of where the player is on screen so its possible to use that ratio to move the camera rather than to do it like this.
+        _playerPosition = _camera.WorldToScreenPoint(player.gameObject.transform.position);
 
-        if (_camera.WorldToScreenPoint(player.gameObject.transform.position).x > Screen.width - screenEdgeW)
+        screenEdgeW = Mathf.FloorToInt((float)Screen.width * (screenPercentX / 2) / 100);
+        screenEdgeH = Mathf.FloorToInt((float)Screen.height * (screenPercentY / 2) / 100);
+
+        if (_playerPosition.x > Screen.width - screenEdgeW)
         {
             _followPosition.x += moveAmount * Time.deltaTime;
             _isCameraMoving = true;
         }
-        if (_camera.WorldToScreenPoint(player.gameObject.transform.position).x < screenEdgeW)
+        if (_playerPosition.x < screenEdgeW)
         {
             _followPosition.x -= moveAmount * Time.deltaTime;
             _isCameraMoving = true;
         }
-        if (_camera.WorldToScreenPoint(player.gameObject.transform.position).y > Screen.height - screenEdgeH)
+        if (_playerPosition.y > Screen.height - screenEdgeH)
         {
             _followPosition.z += moveAmount * Time.deltaTime;
             _isCameraMoving = true;
         }
-        if (_camera.WorldToScreenPoint(player.gameObject.transform.position).y < screenEdgeH)
+        if (_playerPosition.y < screenEdgeH)
         {
             _followPosition.z -= moveAmount * Time.deltaTime;
             _isCameraMoving = true;
         }
+
         transform.position = _followPosition;
     }
 }
