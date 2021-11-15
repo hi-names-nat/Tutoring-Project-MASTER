1be6781e3a59d7735cbff2d9d2fb0cc3066e2a4a
Natalie <natalie.soltis@outlook.com>
2021-11-15 12:44:36 -0500

    Changed some things

--- Assets/Scripts/DamageVolume.cs
+++ Assets/Scripts/DamageVolume.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class DamageVolume : MonoBehaviour
 {
     [SerializeField] int damage = 5;
-
+    [SerializeField] bool doPushBack;
 
     private void OnTriggerEnter(Collider other)
     {
         
+
         if (other.TryGetComponent<standardManagedHealth>(out standardManagedHealth h))
         {
             h.DealHealth(damage, false);
+            if (doPushBack) transform.position -= (transform.forward * 1.5f);
         }
     }
 }
